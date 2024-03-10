ace4a1a8fd170861c1f3a35569e3f290fcf6c625
ygursu <yasemingrs@gmail.com>
2024-03-10 23:38:23 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp133/Program.cs
@@ -0,0 +1,12 @@
+﻿int say = 0, yas = 0;
+
+for (int i = 0; i < 20; i++)
+{
+    Console.Write("{0}. yaşı giriniz:", i + 1);
+    yas = Convert.ToInt16(Console.ReadLine());
+    if (yas >= 18 && yas <= 25)
+        say++;
+}
+
+Console.WriteLine("18 ile 25 yaş arası {0} öğrenci vardır.", say);
+Console.ReadKey();
